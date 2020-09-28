5b98d786377e7316a68d7fdde5a29d2a3de18dfe
sakshi <sakshigpt15@gmail.com>
2020-09-28 16:47:48 -0400

    model experiments

--- 1.code/utils/basic.py
+++ 1.code/utils/basic.py
@@ -1,5 +1,7 @@
-import pandas as pd
+import pandas as pd, numpy as np
 import datetime
+import scipy.stats as ss
+
 
 def convert_to_date(df, col):
     df[col] = pd.to_datetime(df[col])
@@ -12,3 +14,14 @@ def get_date_feats(df, col):
     df[str(col) + '_date'] = df[col].dt.date
     df.drop(columns = col, inplace = True)
     return df
+
+def cramers_v(x, y):
+    confusion_matrix = pd.crosstab(x,y)
+    chi2 = ss.chi2_contingency(confusion_matrix)[0]
+    n = confusion_matrix.sum().sum()
+    phi2 = chi2/n
+    r,k = confusion_matrix.shape
+    phi2corr = max(0, phi2-((k-1)*(r-1))/(n-1))
+    rcorr = r-((r-1)**2)/(n-1)
+    kcorr = k-((k-1)**2)/(n-1)
+    return np.sqrt(phi2corr/min((kcorr-1),(rcorr-1)))
--- 1.code/utils/modeling.py
+++ 1.code/utils/modeling.py
@@ -1,12 +1,16 @@
 from sklearn import metrics
 import numpy  as np
 from sklearn.metrics import r2_score,mean_squared_error
+from sklearn.metrics import mean_squared_log_error
+
 
 def print_result(pred_train, y_train, pred_test, y_test):
     rmse_train = np.sqrt(mean_squared_error(y_train, pred_train))
     r2_train = r2_score(y_train, pred_train)
+    rmsle_train = np.sqrt(mean_squared_log_error(y_train, pred_train))
     rmse_test = np.sqrt(mean_squared_error(y_test, pred_test))
     r2_test = r2_score(y_test, pred_test)
-    print('Train RMSE: {}\nTrain R2: {}\nTest RMSE: {}\nTest R2: {}'.
-         format(rmse_train,r2_train,rmse_test,r2_test))
+    rmsle_test = np.sqrt(mean_squared_log_error(y_test, pred_test))    
+    print('Train RMSE: {}\nTrain R2: {}\nTrain RMSLE: {}\nTest RMSE: {}\nTest R2: {}\nTest RMSLE: {}'.
+         format(rmse_train,r2_train,rmsle_train,rmse_test,r2_test,rmsle_test))
     return
